d98dfd9201dd891ad7de22dbba2b5dfd1246b1aa
Snaipe <franklinmathieu@gmail.com>
2016-08-01 22:03:31 +0200

    sample: Completed timeout sample

--- sample/timeout.c
+++ sample/timeout.c
@@ -15,17 +15,19 @@
 
 static int child(void)
 {
-    for (volatile size_t i = 0; i < 1000000000; ++i);
+    for (;;);
     return 0;
 }
 
 EXPORT int main(void)
 {
     bxf_instance *box;
-    _assert(!bxf_spawn(&box, child));
-    bxf_wait(box, 1);
-    printf("Timed out after 1sec\n");
+    _assert(!bxf_spawn(&box, child, .quotas.runtime = 2.0));
+    bxf_wait(box, 1.0);
+    printf("Wait timed out after 1 second\n");
     bxf_wait(box, BXF_FOREVER);
+    printf("Process killed after %.1f seconds\n", box->time.elapsed / 1000000000.);
     bxf_term(box);
+
     return 0;
 }
