b90a17e639cd6ec1f00be8e00d2b4470725134c7
Snaipe <franklinmathieu@gmail.com>
2016-09-02 22:07:56 +0200

    exe-pe: Follow the relative jmp if the returned address for main is a jump table entry

--- src/exe-pe.c
+++ src/exe-pe.c
@@ -60,6 +60,12 @@ int bxfi_exe_patch_main(bxfi_exe_fn *new_main)
     if (!addr)
         return -1;
 
+#if defined (BXF_ARCH_x86) || defined (BXF_ARCH_x86_64)
+    /* If we got stuck on a jmp table entry we need to follow the trail */
+    if (*(char *)addr == (char)0xE9)
+        addr = (char *)addr + *(DWORD *)((char *) addr + 1) + 5;
+#endif
+
     /* Reserve enough space for the trampoline and copy the default opcodes */
     uintptr_t size = (uintptr_t)&bxfi_trampoline_end
                    - (uintptr_t)&bxfi_trampoline;
