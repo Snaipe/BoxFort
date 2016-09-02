5b10b5186dce47f0b79c79ee887d472aa64b3651
Snaipe <franklinmathieu@gmail.com>
2016-09-02 15:52:05 +0200

    cmake: Added checks for .hidden asm directive

---
+++ .cmake/Modules/CheckCompile.cmake
@@ -0,0 +1,42 @@
+# Copyright (C) 2016 Franklin "Snaipe" Mathieu.
+# Redistribution and use of this file is allowed according to the terms of the MIT license.
+# For details see the LICENSE file distributed with BoxFort.
+
+include (CheckCSourceCompiles)
+
+function (check_type_exists _T _H _VAR)
+  check_c_source_compiles ("
+    #include <${_H}>
+    typedef ${_T} checked_type;
+    int main(void) { return 0; }
+  " ${_VAR})
+endfunction ()
+
+function (check_asm_source_compiles _S _VAR)
+  set (SRC "${PROJECT_BINARY_DIR}/CMakeFiles/CheckASMSource.S")
+  file (WRITE ${SRC} "
+    ${_S}
+    .globl main
+    main:
+  ")
+  try_compile (${_VAR} "${PROJECT_BINARY_DIR}/CMakeFiles" ${SRC}
+    OUTPUT_VARIABLE OUT)
+  if (${_VAR})
+    file (APPEND "${PROJECT_BINARY_DIR}/CMakeFiles/CMakeOutput.log" "${OUT}")
+  else ()
+    file (APPEND "${PROJECT_BINARY_DIR}/CMakeFiles/CMakeError.log" "${OUT}")
+  endif ()
+endfunction ()
+
+function (check_asm_directive_exists _D _VAR)
+  message ("-- Looking whether assembler supports `${_D}`")
+  check_asm_source_compiles ("
+    ${_D}
+    test:
+  " ${_VAR})
+  if (${_VAR})
+    message ("-- Looking whether assembler supports `${_D}` - found")
+  else ()
+    message ("-- Looking whether assembler supports `${_D}` - not found")
+  endif ()
+endfunction ()
