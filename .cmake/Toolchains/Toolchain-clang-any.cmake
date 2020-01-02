3e16c0a51adc1bda6f6b1e84319c89ff95087e8a
Franklin "Snaipe" Mathieu <me@snai.pe>
2020-01-02 20:23:29 +0000

    cmake: remove all files.
    We've switched to meson.

--- .cmake/Toolchains/Toolchain-clang-any.cmake
+++
@@ -1,8 +0,0 @@
-set (CMAKE_SYSTEM_NAME Linux)
-
-set (TRIPLE "arm-linux-gnueabihf")
-
-set(CMAKE_C_COMPILER clang)
-set(CMAKE_C_COMPILER_TARGET ${TRIPLE})
-set(CMAKE_CXX_COMPILER clang++)
-set(CMAKE_CXX_COMPILER_TARGET ${TRIPLE})
